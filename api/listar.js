5f805e475aec98ffb3fd90a9cb6a55d4fd394d69
DanMartinez-kz <danzalumc3@gmail.com>
2026-02-27 13:23:47 -0600

    act27022

--- api/listar.js
+++ api/listar.js
@@ -18,8 +18,9 @@ export default async function handler(req, res) {
 
     const data = await response.json();
     // Solo devolvemos los archivos .json
-    const archivosJson = data.filter(file => file.name.endsWith('.json'));
-    
+   // const archivosJson = data.filter(file => file.name.endsWith('.json'));
+    const archivosJson = data.filter(file => {
+  return file.type === "dir" || file.name.endsWith(".json")});
     res.status(200).json(archivosJson);
   } catch (error) {
     res.status(500).json({ error: error.message });
